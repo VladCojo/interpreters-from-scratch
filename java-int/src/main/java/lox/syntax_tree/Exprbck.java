1e854c46b78aa074a1ecebd90cdf92166a92a85a
VladCojo <vladvcojocaru@gmail.com>
2024-06-01 11:14:10 +0300

    era mai rapid de mana ...

--- java-int/src/main/java/lox/syntax_tree/Exprbck.java
+++ java-int/src/main/java/lox/syntax_tree/Exprbck.java
@@ -2,13 +2,13 @@ package lox.syntax_tree;
 
 import lox.parser.Token;
 
-public abstract class Expr {
-    static class Binary extends Expr {
-        final Expr left;
+public abstract class Exprbck {
+    static class Binary extends Exprbck {
+        final Exprbck left;
         final Token operator;
-        final Expr right;
+        final Exprbck right;
 
-        Binary(Expr left, Token operator, Expr right){
+        Binary(Exprbck left, Token operator, Exprbck right){
             this.left = left;
             this.operator = operator;
             this.right = right;
